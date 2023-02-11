3e08243a96af41e394c008de12a9d2a77f39077c
nabil.alhafez <nabil.alhfz98@gmail.com>
2023-02-11 11:39:45 +0200

    Event Filtering

--- handling-events/handling-events/src/app/courses/courses.component.ts
+++ handling-events/handling-events/src/app/courses/courses.component.ts
@@ -5,14 +5,21 @@ import { Component } from '@angular/core';
   template: `
   <button class="btn btn-primary" [class.active]="isActive" (click)="onSave($event)">Save</button>
   <button class="btn btn-primary" [style.backgroundColor]="isActive ? 'red' : 'grey'">Cancel</button>
-
+  <input (keyup.enter)="onKeyUp()"/> 
   `,
   styleUrls: ['./courses.component.css']
 })
 
 export class CoursesComponent {
   isActive = false;
+
   onSave($event : any){
+    // Stop click bubbling
+    $event.stopPropagation();
     console.log("Button was clicked ", $event)
+  }
+
+  onKeyUp(){
+    console.log("ENTER was pressed ")
   }
 }
