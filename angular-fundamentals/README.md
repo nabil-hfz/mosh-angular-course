11acbc0856b046ff36946528112ceb9510737311
nabil.alhafez <nabil.alhfz98@gmail.com>
2023-02-11 10:23:07 +0200

    Add a new Directive

--- angular-fundamentals/README.md
+++ angular-fundamentals/README.md
@@ -7,3 +7,10 @@
 g:       Stands for generating. 
 c:       Stands for component.
 courser: Stands for component's name.
+
+2- Directives: 
+
+It's a method that is used to manipulate the DOM. Should be always prefixed with *
+For example:
+
+*ngFor="let course of courses"
--- angular-fundamentals/src/app/courses.component.ts
+++ angular-fundamentals/src/app/courses.component.ts
@@ -5,11 +5,20 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'coursers',
-    template: "<h4>{{ getTitle() }}</h4>"
+    template: `<h4>{{ getTitle() }}</h4>
+            <ul>
+                    <li *ngFor="let course of courses"> 
+                    {{ course }}
+                    </li>
+            </ul>
+             
+        `
 })
-export class CoursesComponent{
+export class CoursesComponent {
     title = " to Angular course";
-    getTitle():string{
+    courses = ["course1", "course2", "course3"];
+
+    getTitle(): string {
         return this.title;
     }
 }
