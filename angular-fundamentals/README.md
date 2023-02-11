1ff6e264d282ef89b9fd3fd2dc12820733365c2b
nabil.alhafez <nabil.alhfz98@gmail.com>
2023-02-11 10:07:05 +0200

    Create a new component

---
+++ angular-fundamentals/README.md
@@ -0,0 +1,8 @@
+# AngularFundamentals
+
+1- Create a new component using Angular CLI
+
+`ng g c course` 
+
+g: Stands for generating 
+c: Stands for component 
---
+++ angular-fundamentals/src/app/courses.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+
+
+@Component({
+    selector: "coursers",
+    template: "<h4>Coursers</h4>"
+})
+export class CoursesComponent{
+
+}
