d1378a0b26df0a8c6f660f685410175edd25211b
nabil.alhafez <nabil.alhfz98@gmail.com>
2023-02-11 11:23:54 +0200

    Install bootstrap

---
+++ handling-events/handling-events/README.md
@@ -0,0 +1,7 @@
+# HandlingEvents
+
+1- To install bootstrap with npm:
+
+`npm install bootstrap --save`
+
+save: Stands for add a dep of bootstrap in the current project.
---
+++ handling-events/handling-events/src/app/courses/courses.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-courses',
+  template: `<button class="btn btn-primary">Save</button>`,
+  styleUrls: ['./courses.component.css']
+})
+export class CoursesComponent {
+
+}
